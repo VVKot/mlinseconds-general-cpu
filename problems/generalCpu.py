c3c0c700ca35c4923a190026e3972a47789d48c5
VVKot <volodymyr.kot.ua@gmail.com>
2019-02-10 14:51:46 +0200

    feat: Add basic grid search

--- problems/generalCpu.py
+++ problems/generalCpu.py
@@ -1,39 +1,73 @@
 # You need to learn a function with n inputs.
 # For given number of inputs, we will generate random function.
 # Your task is to learn it
-import time
-import random
-import torch
-import torchvision
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import sys
 sys.path.append('./../utils')
 import solutionmanager as sm
+from gridsearch import GridSearch
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.nn as nn
+import torchvision
+import torch
+import random
+import time
+
 
 class SolutionModel(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, solution):
         super(SolutionModel, self).__init__()
+        self.solution = solution
         self.input_size = input_size
-        sm.SolutionManager.print_hint("Hint[1]: Explore more deep neural networks")
-        self.hidden_size = 10
+        self.hidden_size = solution.hidden_size
+        self.lr = solution.lr
+        self.activation_hidden = solution.activation_hidden
+        self.activation_output = solution.activation_output
+
         self.linear1 = nn.Linear(input_size, self.hidden_size)
         self.linear2 = nn.Linear(self.hidden_size, output_size)
 
     def forward(self, x):
         x = self.linear1(x)
-        x = F.sigmoid(x)
+        x = self.solution.activations[self.activation_hidden](x)
         x = self.linear2(x)
-        x = F.sigmoid(x)
+        x = self.solution.activations[self.activation_output](x)
         return x
+
 
 class Solution():
     def __init__(self):
-        self = self
+        self.hidden_size = 10
+        self.lr = 1.0
+        self.activation_hidden = 'relu'
+        self.activation_output = 'sigmoid'
+        self.activations = {
+            'sigmoid': nn.Sigmoid(),
+            'relu': nn.ReLU(),
+            'relu6': nn.ReLU6(),
+            'rrelu0103': nn.RReLU(0.1, 0.3),
+            'rrelu0205': nn.RReLU(0.2, 0.5),
+            'htang1': nn.Hardtanh(-1, 1),
+            'htang2': nn.Hardtanh(-2, 2),
+            'htang3': nn.Hardtanh(-3, 3),
+            'tanh': nn.Tanh(),
+            'elu': nn.ELU(),
+            'selu': nn.SELU(),
+            'hardshrink': nn.Hardshrink(),
+            'leakyrelu01': nn.LeakyReLU(0.1),
+            'leakyrelu001': nn.LeakyReLU(0.01),
+            'logsigmoid': nn.LogSigmoid(),
+            'prelu': nn.PReLU(),
+        }
+        self.hidden_size_grid = [3, 5, 7, 11]
+        self.lr_grid = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+        self.activation_hidden_grid = [self.activations.keys()]
+        self.activation_output_grid = [self.activations.keys()]
+        self.grid_search = GridSearch(self)
+        self.grid_search.set_enabled(True)
 
     def create_model(self, input_size, output_size):
-        return SolutionModel(input_size, output_size)
+        return SolutionModel(input_size, output_size, self)
 
     # Return number of steps used
     def train_model(self, model, train_data, train_target, context):
@@ -43,15 +77,14 @@ class Solution():
         while True:
             time_left = context.get_timer().get_time_left()
             # No more time left, stop training
-            if time_left < 0.1:
+            if time_left < 0.1 or (model.solution.grid_search.enabled and step > 1000):
                 break
-            optimizer = optim.SGD(model.parameters(), lr=1.0)
+            optimizer = optim.SGD(model.parameters(), lr=model.lr)
             data = train_data
             target = train_target
             # model.parameters()...gradient set to zero
             optimizer.zero_grad()
             # evaluate model => model.forward(data)
-            sm.SolutionManager.print_hint("Hint[2]: Explore other activation functions", step)
             output = model(data)
             # if x < 0.5 predict 0 else predict 1
             predict = output.round()
@@ -60,31 +93,34 @@ class Solution():
             # Total number of needed predictions
             total = target.view(-1).size(0)
             # calculate loss
-            sm.SolutionManager.print_hint("Hint[3]: Explore other loss functions", step)
             loss = ((output-target)**2).sum()
             # calculate deriviative of model.forward() and put it in model.parameters()...gradient
             loss.backward()
             # print progress of the learning
-            self.print_stats(step, loss, correct, total)
+            self.print_stats(step, loss, correct, total, model)
             # update model: model.parameters() -= lr * gradient
             optimizer.step()
             step += 1
         return step
-    
-    def print_stats(self, step, loss, correct, total):
-        if step % 1000 == 0:
-            print("Step = {} Prediction = {}/{} Error = {}".format(step, correct, total, loss.item()))
+
+    def print_stats(self, step, loss, correct, total, model):
+        if step % 200 == 0:
+            print("LR={}, HS={}, ActivHidden={}, ActivOut={}, Step = {} Prediction = {}/{} Error = {}".format(model.lr,
+                                                                                                              model.hidden_size, model.activation_hidden, model.activation_output, step, correct, total, loss.item()))
 
 ###
 ###
-### Don't change code after this line
+# Don't change code after this line
 ###
 ###
+
+
 class Limits:
     def __init__(self):
         self.time_limit = 2.0
         self.size_limit = 10000
         self.test_limit = 1.0
+
 
 class DataProvider:
     def __init__(self):
@@ -97,8 +133,8 @@ class DataProvider:
         target = torch.FloatTensor(data_size)
         for i in range(data_size):
             for j in range(input_size):
-                input_bit = (i>>j)&1
-                data[i,j] = float(input_bit)
+                input_bit = (i >> j) & 1
+                data[i, j] = float(input_bit)
             target[i] = float(random.randint(0, 1))
         return (data, target.view(-1, 1))
 
@@ -118,5 +154,6 @@ class Config:
     def get_solution(self):
         return Solution()
 
+
 # If you want to run specific case, put number here
-sm.SolutionManager(Config()).run(case_number=-1)
+sm.SolutionManager(Config()).run(case_number=1)
